7c59129441aefd1575221117c375cc902b89a63e
Jasmine Quach <xjasminequach@gmail.com>
2017-10-07 21:28:20 -0500

    WIP family hash exercise

---
+++ family_hash.rb
@@ -0,0 +1,26 @@
+my_family = {
+  :brother {
+    :name => 'Dameon',
+    :age => 32
+  },
+  :sister => {
+    :name => 'Holly',
+    :age => 30
+  },
+  :sister {
+    :name => 'Juliet',
+    :age => 29
+  }
+  :brother {
+    :name => 'Conan',
+    :age => 19
+  },
+  :mother {
+    :name => 'Shannon',
+    :age => 55
+  },
+  :father {
+    :name => 'Levitt',
+    :age 56
+  }
+}
